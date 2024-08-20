c256b2b863439db56a88ab814e62251dd1f50741
itsanindyak <iankoley04@gmail.com>
2024-08-20 21:52:39 +0530

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+const subcriptionSchema = new Schema(
+  {
+    subscriber: {
+      Types: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      Types: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subcription = mongoose.model("Subcription", subcriptionSchema);
